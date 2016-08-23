95a0889de1197b660702006e7ae84a4754023bf7
Brice Dobry <brice.dobry@huawei.com>
2016-08-23 15:11:26 +0000

    Change to use version 0.6 of Spectre

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "PathKit",
   dependencies: [
-    .Package(url: "https://github.com/kylef/Spectre", majorVersion: 0, minor: 7),
+    .Package(url: "https://github.com/kylef/Spectre", majorVersion: 0, minor: 6),
   ]
 )
